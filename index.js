abc9dad342fd126972bc957e722deacefc08ad16
Ulisses Albuquerque <ulisses.albuquerque@team.telstra.com>
2017-04-18 13:49:43 +1000

    Added eslint configuration

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 function AccessReferenceMap(options) {
-  const actualOptions = Object.assign({ encoding: this.DEFAULT_ENCODING, width: this.DEFAULT_WIDTH }, options);
+  const actualOptions = Object.assign(this.DEFAULT_OPTIONS, options);
   this.encoding = actualOptions.encoding;
   this.width = actualOptions.width;
   this.dtoi = new Map();
@@ -10,10 +10,14 @@ function AccessReferenceMap(options) {
 
 AccessReferenceMap.prototype.DEFAULT_ENCODING = 'hex';
 AccessReferenceMap.prototype.DEFAULT_WIDTH = 16;
+AccessReferenceMap.prototype.DEFAULT_OPTIONS = {
+  encoding: AccessReferenceMap.prototype.DEFAULT_ENCODING,
+  width: AccessReferenceMap.prototype.DEFAULT_WIDTH,
+};
 
 AccessReferenceMap.prototype.getUniqueReference = function() {
   return crypto.randomBytes(this.width).toString(this.encoding);
-}
+};
 
 AccessReferenceMap.prototype.addDirectReference = function(directReference) {
   if (this.dtoi.has(directReference)) {
@@ -24,35 +28,35 @@ AccessReferenceMap.prototype.addDirectReference = function(directReference) {
   this.itod.set(indirectReference, directReference);
   this.dtoi.set(directReference, indirectReference);
   return indirectReference;
-}
+};
 
 AccessReferenceMap.prototype.removeDirectReference = function(directReference) {
- const indirectReference = this.dtoi.get(directReference);
- if (indirectReference) {
-   this.itod.delete(indirectReference);
-   this.dtoi.delete(directReference);
- }
- return indirectReference;
-}
+  const indirectReference = this.dtoi.get(directReference);
+  if (indirectReference) {
+    this.itod.delete(indirectReference);
+    this.dtoi.delete(directReference);
+  }
+  return indirectReference;
+};
 
 AccessReferenceMap.prototype.update = function(directReferences) {
- this.dtoi.clear();
- this.itod.clear();
- directReferences.forEach((directReference) => {
-   this.addDirectReference(directReference);
- });
-}
+  this.dtoi.clear();
+  this.itod.clear();
+  directReferences.forEach((directReference) => {
+    this.addDirectReference(directReference);
+  });
+};
 
 AccessReferenceMap.prototype.iterator = function() {
- return this.dtoi.keys();
-}
+  return this.dtoi.keys();
+};
 
 AccessReferenceMap.prototype.getDirectReference = function(indirectReference) {
   return this.itod.get(indirectReference);
-}
+};
 
 AccessReferenceMap.prototype.getIndirectReference = function(directReference) {
   return this.dtoi.get(directReference);
-}
+};
 
 module.exports = AccessReferenceMap;
